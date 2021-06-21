55c91c98284bf1e6bcbfa33996ba7d39bf11396d
Yash Gupta <gyash21@gmail.com>
2021-06-21 23:46:07 +0530

    simple pattern programs

---
+++ operators3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0 && n % 3 == 0)
+    {
+        cout << "Divisible by both" << endl;
+    }
+    else if (n % 2 == 0)
+    {
+        cout << "Divisble by 2" << endl;
+    }
+    else if (n % 3 == 0)
+    {
+        cout << "Divisble  by 3" << endl;
+    }
+    else
+    {
+        cout << "Divisible by none" << endl;
+    }
+
+    return 0;
+}
